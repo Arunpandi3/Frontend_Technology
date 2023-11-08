0066e00790dad3cd806e1145d04d6f59a3b3e3e8
Arunpandi3 <146913574+Arunpandi3@users.noreply.github.com>
2023-11-08 10:53:35 +0530

    cat and bulb

---
+++ cat.js
@@ -0,0 +1,12 @@
+// function off(){
+//     document.getElementById("bulbon").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+//     document.getElementById("cat").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+// }
+// function on(){
+//     document.getElementById("bulbon").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+//     document.getElementById("cat").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+// }
+
+var arr=[1,2,3,4,"KGM","empty"];
+console.log(arr);
+
